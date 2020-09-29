eb2f3189401aeb484668a60d7263859becebb425
JaimeRC <jaime.rc@icloud.com>
2020-09-29 21:19:52 +0200

    Add Golang Init Server

--- api-golang/main.go
+++ api-golang/main.go
@@ -1,1 +1,31 @@
-package api_golang
+package main
+
+import (
+	"api-golang/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+
+// Main function
+func main() {
+
+	// Init router
+	r := mux.NewRouter()
+
+	// Add Routes
+	routes.SetBooksRoutes(r)
+	routes.SetHealthCheckRoutes(r)
+
+	// Create Server
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	log.Println("Running on port 8080")
+
+	// Run Server
+	log.Println(srv.ListenAndServe())
+}
