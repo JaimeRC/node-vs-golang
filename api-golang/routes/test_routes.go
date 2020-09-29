4e7c3fbbcd6b24a6a8d355ac0748a654109e2dca
JaimeRC <jaime.rc@icloud.com>
2020-09-29 21:19:32 +0200

    Add Golang Routes

--- api-golang/routes/test_routes.go
+++ api-golang/routes/test_routes.go
@@ -1,1 +1,18 @@
 package routes
+
+import (
+	"api-golang/controllers"
+	"github.com/gorilla/mux"
+)
+
+func SetBooksRoutes(r *mux.Router) {
+
+	subRouter := r.PathPrefix("/api").Subrouter()
+
+	// Route handles & endpoints
+	subRouter.HandleFunc("/test", controllers.GetTests).Methods("GET")
+	subRouter.HandleFunc("/test/{id}", controllers.GetTest).Methods("GET")
+	subRouter.HandleFunc("/test", controllers.CreateTest).Methods("POST")
+	subRouter.HandleFunc("/test/{id}", controllers.UpdateTest).Methods("PUT")
+	subRouter.HandleFunc("/test/{id}", controllers.DeleteTest).Methods("DELETE")
+}
